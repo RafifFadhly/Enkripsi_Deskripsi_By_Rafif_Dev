7a745caa3134e48f3104e89f818c144d8aadbdc7
RafifFadhly <rafiffadhly@gmail.com>
2024-05-04 17:53:32 +0700

    tes

---
+++ Extention Enkripsi Deskripsi.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace Encrypt
+{
+    public static class CaesarCipher
+    {
+        public const string PrintableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~ ";
+
+        private static int _shift;
+
+        public static void SetShift(int shift)
+        {
+            _shift = (shift % PrintableChars.Length + PrintableChars.Length) % PrintableChars.Length;
+        }
+
+        public static string Encrypt(string input)
+        {
+            StringBuilder encrypted = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                int index = PrintableChars.IndexOf(c);
+                if (index != -1)
+                {
+                    int newIndex = (index + _shift) % PrintableChars.Length;
+                    encrypted.Append(PrintableChars[newIndex]);
+                }
+                else
+                {
+                    encrypted.Append(c);
+                }
+            }
+
+            return encrypted.ToString();
+        }
+
+        public static string Decrypt(string input)
+        {
+            StringBuilder decrypted = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                int index = PrintableChars.IndexOf(c);
+                if (index != -1)
+                {
+                    int newIndex = (index - _shift + PrintableChars.Length) % PrintableChars.Length;
+                    decrypted.Append(PrintableChars[newIndex]);
+                }
+                else
+                {
+                    decrypted.Append(c);
+                }
+            }
+
+            return decrypted.ToString();
+        }
+    }
+}
